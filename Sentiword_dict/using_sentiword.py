eb7b3da5dd42b656f6d21e320a9f0da7ab7bd258
brownnyi <brown970523@naver.com>
2024-09-05 13:05:30 +0900

    Update using_sentiword.py

--- Sentiword_dict/using_sentiword.py
+++ Sentiword_dict/using_sentiword.py
@@ -10,3 +10,18 @@ for token in tokens:
             sentiment += int(sentiword_dic['polarity'][i])  
     df.loc[idx] = [token, sentiment]  
     idx += 1
+
+def label_sentiment(sentiment):
+    if sentiment > 0:
+        return '긍정적'
+    elif sentiment < 0:
+        return '부정적'
+    else:
+        return '중립적'
+
+# '감정' 컬럼을 추가
+df['감정'] = df['sentiment'].apply(label_sentiment)
+
+mid = len(df[df['감정'] == '중립적'])
+neg = len(df[df['감정'] == '부정적'])
+pos = len(df[df['감정'] == '긍정적'])
